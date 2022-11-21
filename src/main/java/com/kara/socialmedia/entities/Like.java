c28c4840c67830ade92a6d5a83c09fe4869579cd
Hatice Karatay <haticekaraty@gmail.com>
2022-11-20 19:53:16 -0500

    add relationships to entities

--- src/main/java/com/kara/socialmedia/entities/Like.java
+++ src/main/java/com/kara/socialmedia/entities/Like.java
@@ -1,10 +1,11 @@
 package com.kara.socialmedia.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "p_like")
@@ -12,6 +13,15 @@ import javax.persistence.Table;
 public class Like {
     @Id
     Long id;
-    Long postId;
-    Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE) //upon deletion of a user, remove all related posts
+    @JsonIgnore
+    User user;
 }
